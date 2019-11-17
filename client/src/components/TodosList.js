80e958e15f4792c21a62a854d09cad2d0bf2835c
dbhuller <dbhuller@mail.sfsu.edu>
2019-11-17 03:42:51 -0800

    added state hooks to TodoList

--- client/src/components/TodosList.js
+++ client/src/components/TodosList.js
@@ -1,7 +1,23 @@
 import React from 'react';
 
 
-function TodosList() {
+
+function TodosList(props) {
+
+    const [description, setDescription] = React.useState('');
+    const [author, setAuthor] = React.useState('');
+    const [priority, setPriority] = React.useState('');
+    const [completed, setCompleted] = React.useState(false);
+
+    /*
+    let state = {
+        todo_description: '',
+        todo_author: '',
+        todo_priority: '',
+        todo_completed: false
+    }
+    */
+
     return (
         <div>
             <p>Todo List Component</p>
