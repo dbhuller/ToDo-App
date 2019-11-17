7ed04e8bb9208b0d002db3a4df67166d4b8aac9d
dbhuller <dbhuller@mail.sfsu.edu>
2019-11-17 12:55:56 -0800

    added priority radio buttons in CreateTodo

--- client/src/components/CreateTodo.js
+++ client/src/components/CreateTodo.js
@@ -32,6 +32,44 @@ function CreateTodo() {
                     onChange={e => setAuthor(e.target.value)}
                     />
                 </div>
+                <div className='form-group'>
+                    <div className='form-check form-check-inline'>
+                        <input
+                        className='form-check-input'
+                        type='radio'
+                        name='priority'
+                        id='priorityLow'
+                        value='Low'
+                        checked={priority === 'Low'}
+                        onChange={e => setPriority('Low')}
+                        />
+                        <label className='form-check-label'>Low</label>
+                    </div>
+                    <div className='form-check form-check-inline'>
+                        <input
+                        className='form-check-input'
+                        type='radio'
+                        name='priority'
+                        id='priorityMed'
+                        value='Medium'
+                        checked={priority === 'Medium'}
+                        onChange={e => setPriority('Medium')}
+                        />
+                        <label className='form-check-label'>Medium</label>
+                    </div>
+                    <div className='form-check form-check-inline'>
+                        <input
+                        className='form-check-input'
+                        type='radio'
+                        name='priority'
+                        id='priorityHigh'
+                        value='High'
+                        checked={priority === 'High'}
+                        onChange={e => setPriority('High')}
+                        />
+                        <label className='form-check-label'>High</label>
+                    </div>
+                </div>
             </form>
         </div>
     )
