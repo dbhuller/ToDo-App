c2d7d001954256b77baf935cb4b6af80e3468581
dbhuller <dbhuller@mail.sfsu.edu>
2019-11-17 02:01:38 -0800

    initial boilerplate and set-up

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+//added imports
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className='container'>
+        <h2>ToDo-App</h2>
+      </div>
+    </div>
+  );
+}
+
+export default App;
